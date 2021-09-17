517bcaa1e0a3f463bb3b36e745a0bed7efe9cecb
kristiansamdal <samd@itu.dk>
2021-09-17 13:02:19 +0200

    project

---
+++ Assignment2Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Assignment2;
+
+namespace Assignment2Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
