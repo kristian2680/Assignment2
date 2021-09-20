750f0a8830a5f4182618f6051a7f7ced68668683
kristian2680 <90180990+kristian2680@users.noreply.github.com>
2021-09-20 17:19:18 +0200

    final code product (#1)
    Co-authored-by: kristiansamdal <samd@itu.dk>

--- Assignment2/Student.cs
+++ Assignment2/Student.cs
@@ -2,63 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace Assignment2Test
+namespace Assignment2
 {
     public enum Status {New, Active, Dropout, Graduated};
-    public class student
+    public class Student
     {
 
-        int Id {get; init;}
-        string GivenName;
-        string SurName;
-        
-        Status status {get; set;}
-        DateTime StartDate;
-        DateTime EndDate;
-        DateTime GraduationDate;
+        public int Id {get; init;}
+        public string GivenName {get; set;}
+        public string SurName {get; set;}        
+        public Status status {get => calculateStatus(DateTime.Now, this);}
+        public DateTime StartDate {get; set;}
+        public DateTime EndDate {get; set;}
+        public DateTime GraduationDate {get; set;}
 
-
-        public Student (int id, string givenName, string surName, DateTime startDate, DateTime endDate, DateTime graduationDate){
-            Id = id;
-            GivenName = givenName;
-            SurName = surName;
-            StartDate = startDate;
-            EndDate = endDate;
-            GraduationDate = graduationDate;
-
-            if(startDate<endDate && startDate<graduationDate && endDate>=graduationDate){
-                if(DateTime.Now < startDate){
-                    status = Status.New;
+        public static Status calculateStatus(DateTime now, Student s){
+            if(s.StartDate<s.EndDate && s.StartDate<s.GraduationDate && s.EndDate>=s.GraduationDate){
+                if(now < s.StartDate){
+                    return Status.New;
                 }
-                else if(DateTime.Now > startDate){
-                    status = Status.Active;
+                else if(now > s.StartDate){
+                    return Status.Active;
                 }
             }
-            else if (endDate<graduationDate) {
-                status = Status.Dropout;
+            else if (s.EndDate<s.GraduationDate) {
+                return Status.Dropout;
             }
             else{
-                status = Status.Graduated;
+                return Status.Graduated;
             }
-
-
+            throw new Exception();
         }
 
         public string toString(){
-            var ts = "Name: " + this.GivenName + " Id: " + this.Id + " Status: " + this.status + " Startdate: " + this.StartDate + " Enddate: " + this.EndDate + " Graduationdate: " + this.GraduationDate;
-            Console.WriteLine(ts);
+            var ts = "Name: " + this.GivenName + ". Surname: " + this.SurName + ". Id: " + this.Id + ". Status: " + this.status + ". Startdate: " + this.StartDate + ". Enddate: " + this.EndDate + ". Graduationdate: " + this.GraduationDate;
             return ts;
         }
-
-
-        public static void main (string [] args){
-            Student student =  new Student(12, 
-            "Hej", 
-            "med dig", 
-            startdate: new DateTime(year: 2021, month: 9, day: 17),
-            endDate: new DateTime(year: 2021, month: 10, day: 21),
-            graduationDate: new DateTime(year: 2022, month: 1, day: 10));
-        }
-
     }
 }
--- Assignment2Test/UnitTest1.cs
+++ Assignment2Test/UnitTest1.cs
@@ -7,9 +7,58 @@ namespace Assignment2Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void testing_toString_method_Student()
         {
 
+            Student student=new Student{GivenName="Hej", 
+            SurName="med dig", 
+            Id=12, 
+            StartDate=new DateTime(year: 2021, month: 9, day: 17, hour: 12, minute: 00, second: 00), 
+            EndDate=new DateTime(year: 2022, month: 1, day: 9, hour: 12, minute: 00, second: 00), 
+            GraduationDate=new DateTime(year: 2022, month: 1, day: 10, hour: 12, minute: 00, second: 00)};
+
+            var expected = "Name: Hej. Surname: med dig. Id: 12. Status: Dropout. Startdate: 17.09.2021 12.00.00. Enddate: 09.01.2022 12.00.00. Graduationdate: 10.01.2022 12.00.00";
+            var actual = student.toString();
+
+            Assert.Equal(expected, actual);
         }
+
+    
+
+        [Fact]
+        public void calculateStatus_check_status_DropOut(){
+            Student student=new Student{GivenName="Hej", 
+            SurName="med dig", 
+            Id=12, 
+            StartDate=new DateTime(year: 2021, month: 9, day: 17, hour: 12, minute: 00, second: 00), 
+            EndDate=new DateTime(year: 2022, month: 1, day: 9, hour: 12, minute: 00, second: 00), 
+            GraduationDate=new DateTime(year: 2022, month: 1, day: 10, hour: 12, minute: 00, second: 00)};
+
+
+            var actual = student.status;
+            var expected = Status.Dropout;
+
+            Assert.Equal(expected, actual);
+        }
+
+         [Fact]
+        public void calculateStatus_check_status_Active(){
+            Student student=new Student{GivenName="Hej", 
+            SurName="med dig", 
+            Id=21, 
+            StartDate=new DateTime(year: 2021, month: 9, day: 17, hour: 12, minute: 00, second: 00), 
+            EndDate=new DateTime(year: 2023, month: 1, day: 9, hour: 12, minute: 00, second: 00), 
+            GraduationDate=new DateTime(year: 2022, month: 1, day: 10, hour: 12, minute: 00, second: 00)};
+
+
+            var actual = student.status;
+            var expected = Status.Active;
+
+            Assert.Equal(expected, actual);
+        }
+
+        
+
+
     }
 }
